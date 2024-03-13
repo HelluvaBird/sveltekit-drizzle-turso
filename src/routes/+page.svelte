dd606434299db7404d1204986bf996380905393a
HelluvaBird <royceman24@hotmail.com>
2024-03-12 20:26:59 -0400

    add some basic style... should have just installed tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
 <h1>Welcome to SvelteKit - Drizzle - Turso</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid">
+	<a href="/create">Create</a>
+	<a href="/read">Read</a>
+	<a href="/update">Update</a>
+	<a href="/delete">Delete</a>
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		max-width: 1280px;
+		justify-items: start;
+		justify-content: start;
+		gap: 2rem;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		.grid {
+			grid-template-columns: repeat(1, minmax(0, 1fr));
+		}
+	}
+	@media (min-width: 768px) {
+		.grid {
+			grid-template-columns: repeat(2, minmax(0, 1fr));
+		}
+	}
+	@media (min-width: 1024px) {
+		.grid {
+			grid-template-columns: repeat(4, auto);
+		}
+	}
+</style>
